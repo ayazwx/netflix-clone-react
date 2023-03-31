616bcc7a186f576f7fd1a4a37e4a3045610c4ce1
Vefa Ayaz <vefaayazwx@gmail.com>
2023-03-31 19:47:05 +0300

    firebase api renewed

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,15 +1,20 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD9QXgNq2MlHL71b8dwZLHMDzfFoKydN0Q",
-  authDomain: "react-netflix-clone-ec26e.firebaseapp.com",
-  projectId: "react-netflix-clone-ec26e",
-  storageBucket: "react-netflix-clone-ec26e.appspot.com",
-  messagingSenderId: "1017440402197",
-  appId: "1:1017440402197:web:6d535644dc69374f2b071c",
-  measurementId: "G-22GFC276S4"
+  apiKey: "AIzaSyBdIyNw4GqVERHvZOq-fdRqfsD3X-xU0Lc",
+  authDomain: "netflix-clone-304f1.firebaseapp.com",
+  projectId: "netflix-clone-304f1",
+  storageBucket: "netflix-clone-304f1.appspot.com",
+  messagingSenderId: "1022444536200",
+  appId: "1:1022444536200:web:39c469059eb775e1fbba3d"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
